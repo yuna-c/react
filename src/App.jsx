22ff944cdc86f5e870044dc00685b1e253451680
yuna <yuna744@gmail.com>
2024-08-09 21:52:16 +0900

    📝 add : JSX map 개수 만큼 뽑아내기

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import './App.scss';
 
 function App() {
 	const number = 1000;
+	const arr = [1, 2, 3, 4, 5];
 	return (
 		<>
 			<div>1</div>
 			<div>{number}</div>
+			{arr.map((num, idx) => {
+				return <div key={idx}>{num}</div>;
+			})}
 		</>
 	);
 }
