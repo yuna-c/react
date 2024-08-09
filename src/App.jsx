d98f3c1b394f95422299cd2d5c67e4ff2c6c7a07
yuna <yuna744@gmail.com>
2024-08-10 06:44:12 +0900

    📝 add : 추가/삭제를 위한 유니크 아이디 만들기

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,6 @@ import './styles/App.scss';
 // : 반복되는 컴포넌트 처리하는
 
 const App = () => {
-	const [toggle, setToggle] = useState(false);
-	const onShowButton = () => {
-		setToggle(!toggle);
-		console.log(toggle);
-	};
-	const vegetables = ['감자', '고구마', '어이', '가지', '없어'];
-	const users = [
-		{ id: 1, age: 32, name: '아이유' },
-		{ id: 2, age: 32, name: '민지' },
-		{ id: 3, age: 32, name: '수지' },
-		{ id: 4, age: 32, name: '김사랑' },
-		{ id: 5, age: 32, name: '유나' },
-	];
-
 	const styles = {
 		minHeight: 'calc( 100vh - 10rem)',
 		listStyle: 'none',
@@ -28,39 +14,30 @@ const App = () => {
 		display: 'flex',
 		gap: '20px',
 	};
-	const boxStyle = {
-		width: '100px',
-		height: '100px',
-		display: 'flex',
-		alignItems: 'center',
-		justifyContent: 'center',
-		border: '1px solid #000',
-		borderRadius: '3px',
-		backgroundColor: '#eac6c6',
-	};
+
+	// 추가 삭제를 위해 state에 담아 상태값 만들기
+	// ID값의 절대값 대신 유니크한 값으로 변경 new Date().getTime() + n 처리
+	const [users, setUsers] = useState([
+		{ id: new Date().getTime(), age: 32, name: '아이유' },
+		{ id: new Date().getTime() + 1, age: 22, name: '민지' },
+		{ id: new Date().getTime() + 2, age: 32, name: '수지' },
+		{ id: new Date().getTime() + 3, age: 39, name: '김사랑' },
+		{ id: new Date().getTime() + 4, age: 29, name: '유나' },
+	]);
+	console.log(users);
 
 	return (
 		<>
-			<button onClick={onShowButton}>toggle</button>
-			{!toggle ? (
-				<ul style={styles}>
-					{vegetables.map((vage, idx) => (
-						<li key={vage + idx} style={boxStyle}>
-							{vage}
-						</li>
-					))}
-				</ul>
-			) : (
-				<ul style={styles}>
-					{users.map((user, idx) => (
-						<User id={idx} key={user.id} user={user} />
-					))}
-				</ul>
-			)}
+			<ul style={styles}>
+				{users.map((user, idx) => (
+					<User id={idx} key={user.id} user={user} />
+				))}
+			</ul>
 		</>
 	);
 };
 
+// User 컴포넌트를 분리해서 구현
 const User = ({ user, idx }) => {
 	const boxStyle = {
 		width: '100px',
@@ -70,14 +47,14 @@ const User = ({ user, idx }) => {
 		justifyContent: 'center',
 		border: '1px solid #000',
 		borderRadius: '3px',
-		backgroundColor: '#9adce6',
+		backgroundColor: '#ceebef',
 	};
 
 	const { age, name } = user;
 
 	return (
 		<li id={idx} key={user.id} style={boxStyle}>
-			{name} - {age}살
+			{name} <br /> {age}살
 		</li>
 	);
 };
