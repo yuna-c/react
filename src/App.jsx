cb3758c09999e107e65c694672fff181b1ac9be7
yuna <yuna744@gmail.com>
2024-08-09 21:21:24 +0900

    📝 add : 닫는 태그 필수

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.scss';
-// JSX는 가상의 react 돔
-// barbel로 jsx -> js로 변환
-
-// 스니펫 : Simple React Snippets, ES7 + React/ Redux, React-Native Snippets
-// rafce
 
 function App() {
-	return <div></div>;
+	return (
+		<div>
+			<input type='text' />
+			<button></button>
+		</div>
+	);
 }
 
 export default App;
