d0f4b6f3a8c84eb30fbaa74e56cc197ce800d405
yuna <yuna744@gmail.com>
2024-08-09 21:56:04 +0900

    📝 add : style = {{color : "red",}}

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,15 @@ function App() {
 	return (
 		<>
 			<div id='id' className='class'>
-				1
+				{/* JS요소가 들어가니 중괄호 1, 객체가 들어가야 하니 중괄호 2 */}
+				<p
+					style={{
+						color: 'red',
+						fontSize: '20px',
+					}}
+				>
+					red
+				</p>
 			</div>
 		</>
 	);
