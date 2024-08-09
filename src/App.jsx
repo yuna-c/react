d973cde58101f9f0e0a6c6568f9b783e438e8165
yuna <yuna744@gmail.com>
2024-08-09 21:57:04 +0900

    📝 add : 스타일 객체 변수

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import './App.scss';
 
 function App() {
+	const styles = {
+		color: 'blue',
+		fontSize: '30px',
+	};
 	return (
 		<>
 			<div id='id' className='class'>
 				{/* JS요소가 들어가니 중괄호 1, 객체가 들어가야 하니 중괄호 2 */}
-				<p
-					style={{
-						color: 'red',
-						fontSize: '20px',
-					}}
-				>
-					red
-				</p>
+				<p style={styles}>red</p>
 			</div>
 		</>
 	);
