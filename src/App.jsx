fba59f3f9845e9274560c8dab8c7bcf67f7976a2
yuna <yuna744@gmail.com>
2024-08-09 23:29:47 +0900

    📝 add : 자식 컴포넌트에 명시적으로 props 넘기기

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,13 @@
 import './App.scss';
 
-function GrandFather() {
-	const name = '김할아범';
-	return <Mother name={name} />;
-}
-
-// prop drilling
-// : 데이터를 계속 내려주는 현상
-// : redux에서는 피해야함
-function Mother(props) {
-	const name = props.name;
-	return <Child name={name} />;
-}
-
-function Child(props) {
-	const name = props.name;
-	return <div>{name}의 손자 르탄이</div>;
-}
-
+const User = (props) => {
+	console.log(`props =>`, props);
+	console.log(`props =>`, { ...props });
+	return <div>유저님 {props.children}</div>;
+};
 function App() {
-	// props
-	// : 부모 컴포넌트가 자식 컴포넌트에게 물려준 데이터
-	// : 컴포넌트 간의 정보 교류 방법
-	// : props는 반드시 위에서 아래 방향 [부모]-> 자식 : 단방향
-	// : props는 반드시 읽기 전용으로 취급하며, 변경X
-
-	return (
-		<div>
-			<GrandFather />
-		</div>
-	);
+	return <User abc='124'>안녕하세요!!!!!!!!!</User>;
+	// User 컴포넌트 안의 값이 chidren안쪽으로 들어간다
 }
 
 export default App;
