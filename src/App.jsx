2741f8aaa5054cd0bfcc7d007fa62a2afb521c5d
yuna <yuna744@gmail.com>
2024-08-10 04:27:08 +0900

    📝 add : useState 불변성을 유지하는 코드 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import { useState } from 'react';
 import './styles/App.scss';
 
-// 불변성 : 불변성이란 메모리에 있는 값을 변경할 수 없는 것
-// 참조형 변수(객체,배열,함수) 불변성 X
-// 리렌더링 여부 : state의 변화
-// 리액트에서 원시데이터가 아닌 데이터를 수정할 때 불변성을 지켜주지 않고, 직접 수정을 가하면 값은 바뀌지만 메모리주소는 변함이 없게 되는것
 const App = () => {
-	const [dogs, setDogs] = useState(['말티즈']);
-	const onClickHandler = () => {
-		// dogs.push('시고르자브종'); 값이 바꼈다고 인지를 못함
-		setDogs([...dogs, '시고르자브종']); //배열을 새로 만들고 강아지를 넣어줌
-		console.log(dogs);
+	const [items, setItems] = useState([1, 2, 3]);
+	const addItem = () => {
+		// items.push(items.length + 1); // 배열에 직접 push, 불변성 위반
+		// setItems(items); // 상태 업데이트
+		setItems([...items, items.length + 1]);
 	};
 	return (
-		<>
-			<button onClick={onClickHandler}>버튼</button>
-		</>
+		<div>
+			<button onClick={addItem}>Add Item</button>
+			<ul>
+				{items.map((item, index) => (
+					<li key={index}>{item}</li>
+				))}
+			</ul>
+		</div>
 	);
 };
 export default App;
