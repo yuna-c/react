028d452baba11a70a7db2daf2b20bff99b563ada
yuna <yuna744@gmail.com>
2024-08-15 23:21:29 +0900

    📝 add : refactoring styled-components (조건부)

--- src/App.jsx
+++ src/App.jsx
@@ -12,28 +12,39 @@ import styled from 'styled-components';
 const StBox = styled.div`
   width: 120px;
   height: 120px;
-  border: 1px solid ${(props) => props.borderColor}; // 4.부모 컴포넌트에서 보낸 props를 받아 사용
+  border: 1px solid ${(props) => props.borderColor};
   margin: 20px;
   padding: 20px;
 `;
 
-const Stp = styled.p`
-  color: purple;
-  font-weight: 700;
-`;
+const boxList = ['red', 'green', 'blue'];
+const getBoxName = (color) => {
+  switch (color) {
+    case 'red':
+      return '빨간 박스';
+    case 'green':
+      return '초록 박스';
+    case 'blue':
+      return '파란 박스';
+    default:
+      return '검정 박스';
+  }
+};
+
 const App = () => {
   return (
     <>
-      <h1 style={{ color: 'blue' }}>Hello World</h1> {/* style 객체 key:value pair */}
-      <p className='sample'>this is simple app</p> {/* style class */}
-      {/* 2. 위에서 만든 styled-components를 사용 */}
-      {/* 3. props를 통해 borderColor라는 값을 전달 */}
-      <StBox borderColor='red'>빨간 박스</StBox>
+      {/* <StBox borderColor='red'>빨간 박스</StBox>
       <StBox borderColor='green'>초록 박스</StBox>
-      <StBox borderColor='blue'>
-        파란박스
-        <Stp>스타일이 적용된 P tag</Stp>
-      </StBox>
+      <StBox borderColor='blue'>파란 박스</StBox> */}
+
+      {boxList.map((boxColor) => {
+        return (
+          <StBox key={boxColor} borderColor={boxColor}>
+            {getBoxName(boxColor)}
+          </StBox>
+        );
+      })}
     </>
   );
 };
