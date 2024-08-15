561853a8214bdc31e4c86cbd35ab456d924e1d0b
yuna <yuna744@gmail.com>
2024-08-15 23:28:30 +0900

    📝 add : reset css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
-import GlobalStyle from './GlobalStyle';
-import TestPage from './components/TestPage';
-import BlogPost from './components/BlogPost';
-
 import './styles/App.css';
 
 // 🌞🌞🌞🌞🌞
@@ -9,14 +5,9 @@ import './styles/App.css';
 // 확장자 Styled-Components Extractor 설치
 
 const App = () => {
-  const title = '전역 스타일링 제목입니다.';
-  const contents = '전역 스타일링 내용입니다.';
-
   return (
     <>
-      <GlobalStyle />
-      <TestPage title='제목입니다.' contents='내용입니다.' />
-      <BlogPost title={title} contents={contents} />
+      <h1>테스트입니다.</h1>
     </>
   );
 };
