6ec853c21ecb8ae5b45e4d234983d2630cc0cfed
yuna <yuna744@gmail.com>
2024-08-16 01:47:47 +0900

    📝 add : useEffect clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,28 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import './styles/App.css';
 
 // 🌞🌞🌞🌞🌞
 // useEffect
 // 리액트 컴포넌트가 렌더링 된 이후마다 특정 작업을 수행하도록 설정할 수 있는 Hook
 // 어떤 컴포넌트가 화면에 보여졌을 때 내가 무언가를 실행하고 싶을 때, 어떤 컴포넌트가 화면에서 사라졌을 때 무언가를 실행하고 싶을 때
-
-// strict mode : 콘솔이 2번 찍혀 있는 것이 보이는 이유 => strict mode를 제거하는 방법은 main.jsx에서 <React.StrictMode> 부분을 제거
+// 함수형 컴포넌트를 사용할 때는 useEffect를 주로 사용하여 핸들링
 
 const App = () => {
-  const [value, setValue] = useState('');
-  const [count, setCount] = useState(0);
-
-  // setValue가 계속 변하며 리랜더링 되며 계속 useEffect 호출되기 때문에
-  // useEffect(() => {console.log('hello useEffect')});
-
-  // 의존성 배열 → 의존성 배열이 빈 배열인 경우 : 최초 렌더링 이후 딱 한번만 실행되고 그 이후로는 어떤일이 일어나도 실행X
-  // useEffect(() => {console.log('hello useEffect')}, []);
-
-  // 의존성 배열 → 의존성 배열에 값이 있는 경우 : 렌더링 후 배열에 값을 넣으면 그 값(trriger)이 바뀔 때만 useEffect을 실행
   useEffect(() => {
-    console.log('hello useEffect');
-  }, [count]);
+    // 화면에 컴포넌트가 나타났을(mount) 때 실행하고자 하는 함수를 넣어주세요.
+    console.log('Soksea');
+
+    // clean up 함수 : 리소스 정리, 메모리 누수 방지,  unmount될 때, 컴포넌트가 안보일 때 사용
+    return () => {
+      // 화면에서 컴포넌트가 사라졌을(unmount) 때 실행하고자 하는 함수를 넣어주세요.
+      console.log('안녕히 계세요 여러분! 전 이 세상의 모든 굴레와 속박을 벗어 던지고 제 행복을 찾아 떠납니다! 여러분도 행복하세요~~!');
+    };
+  }, []);
 
   return (
     <>
       <h1>useEffect</h1>
-      <input
-        type='text'
-        value={value}
-        onChange={(e) => {
-          setValue(e.target.value);
-          console.log('value => ', value);
-        }}
-      />
       <br />
-      <br />
-      <br />
-
-      {count}
-      <br />
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        증가
-      </button>
     </>
   );
 };
