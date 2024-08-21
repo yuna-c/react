cbe688adc3cb9fbc68c869a1034d3bfee2567fe0
yuna <yuna744@gmail.com>
2024-08-21 11:18:48 +0900

    📝 add : 수업 방향

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-// import "./styles/reset.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './styles/reset.css';
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>
 );
+
+/*
+1-1 styled-component
+1-3 useState
+1-4 useEffect
+1-5 useContext(provider)
+1-6 useref(무한 스크롤)
+1-7 memozation(깜빡임)
+1-8 customHook
+
+1-10 redux(리덕스 툴킷)
+1-16 react router dom
+1-17 react router dom
+1-18 react router dom
+*/
