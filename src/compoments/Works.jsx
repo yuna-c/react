80d5d187633a2bb74092d1224aed1e6ac21c3cc9
yuna <yuna744@gmail.com>
2024-08-22 03:11:38 +0900

    📝 add : Link

--- src/compoments/Works.jsx
+++ src/compoments/Works.jsx
@@ -1,4 +1,4 @@
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate, useLocation, Link } from 'react-router-dom';
 
 const Works = () => {
   const navigate = useNavigate();
@@ -15,6 +15,9 @@ const Works = () => {
       <h1>Works 페이지 입니다.</h1>
       <div>현재페이지 : {location.pathname.slice(1)}</div>
       <button onClick={goToHome}>Home로 이동</button>
+      <Link style={{ color: 'red' }} to="/contact">
+        Contact 페이지로 이동하기
+      </Link>
     </div>
   );
 };
