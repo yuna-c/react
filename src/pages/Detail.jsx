4e48cb50f37dd4dab1329008b124da6b27acb864
yuna <yuna744@gmail.com>
2024-08-21 12:25:15 +0900

    📝 add : useSearchParams

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useSearchParams } from 'react-router-dom';
 
 const Detail = () => {
   const 변수이름아무거나 = useParams();
@@ -6,10 +6,19 @@ const Detail = () => {
 
   const 이름 = 변수이름아무거나.id;
   console.log(이름);
+
+  const [쿼리파람, set쿼리파람] = useSearchParams();
+  console.log(쿼리파람);
+
+  // get 필수 => ?age=20
+  const 나이 = 쿼리파람.get('age');
+  console.log(나이);
+
   return (
     <>
       <div className="Detail">
-        <h3>${이름}님 안녕하세요</h3>
+        <h3>{이름}님 안녕하세요</h3>
+        <button onClick={() => set쿼리파람('age=50')}>파람 바꾸기</button>
       </div>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,15 @@ import { useNavigate } from 'react-router-dom';
 
 const home = () => {
   const navigate = useNavigate();
+
+  const goToDetailPage = (id) => {
+    navigate(`/detail/${id}`); // 예: /detail/1
+  };
   return (
     <div className="home">
       <h3>home 입니다</h3>
-      <button onClick={() => navigate('/Gt')}>갓탠다드 페이지로 갈게요!!</button>
-      <button onClick={() => navigate('/Detail')}>Detail</button>
+      <button onClick={() => navigate('/gt')}>갓탠다드 페이지로 갈게요!!</button>
+      <button onClick={() => goToDetailPage('김르탄')}>Detail</button>
     </div>
   );
 };
