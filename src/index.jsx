4fde7c7229263276e2aa7a3ffd16d9eddbc7f778
yuna <yuna744@gmail.com>
2024-09-06 20:17:21 +0900

    📝 add : json setting

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import './styles/index.css';
-
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './styles/index.css'
+// yarn add json-server
+// yarn json-server db.json --port 4000
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>
-);
+)
