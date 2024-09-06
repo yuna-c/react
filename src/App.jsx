7f0473c0c44b901b554f3456fea3cff5da3bc493
yuna <yuna744@gmail.com>
2024-09-06 20:28:43 +0900

    📝 add : axios get

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-import './styles/App.css';
+import './styles/App.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+// https://axios-http.com/kr/docs/req_config
+// axios.get(url[, config]) : GET(url은 매개변수, 대괄호([])
 
 const App = () => {
-  return <></>;
-};
+  const apiKey = import.meta.env.VITE_API_KEY
+  const server = import.meta.env.VITE_EXAMPLE_SERVER_URL
+  const secret = import.meta.env.VITE_SECRET_KEY
+  console.log('API Key:', apiKey)
+  console.log('SERVER Url:', server)
+  console.log('SECRET Key:', secret)
 
-export default App;
+  const [todos, setTodos] = useState(null)
+
+  const fetchTodos = async () => {
+    const { data } = await axios.get('http://localhost:4000/todos')
+    setTodos(data)
+  }
+
+  useEffect(() => {
+    fetchTodos()
+  }, [])
+
+  console.log('todos=> ', todos)
+  return <div>App</div>
+}
+
+export default App
--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,9 @@ import App from './App.jsx'
 import './styles/index.css'
 // yarn add json-server
 // yarn json-server db.json --port 4000
+
+// yarn add axios
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
