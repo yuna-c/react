4aa942b96b42ff04fe9ad2a25e96afa9fd9efda7
yuna <yuna744@gmail.com>
2024-09-06 22:55:18 +0900

    📝 add : useMutation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import { useMutation, useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 // import Async from './components/async'
 import './styles/App.css'
-import { useQuery } from '@tanstack/react-query'
+import { useState } from 'react'
 
 // NOTE - TanStack Query
 
@@ -12,11 +13,18 @@ import { useQuery } from '@tanstack/react-query'
 // 주요 기능 : 데이터 캐싱, 자동 리페칭, 쿼리 무효화
 
 // NOTE - useQuery : 데이터를 가져오기(쿼리 키와 비동기 함수(패칭 함수))
+// NOTE - useMutation : 데이터를 생성, 수정, 삭제하는 등의 작업에 사용되는 훅([비동기 작업]을 쉽게 처리한다는 말 안에는 [작업이 완료된 후에 관련된 쿼리를 무효화]하는 과정이 포함)
 
 const App = () => {
+  const [todoItem, setTodoItem] = useState('')
+
   const fetchTodos = async () => {
     const response = await axios.get('http://localhost:4000/todos')
     return response.data
+  }
+
+  const addTodo = async (newTodo) => {
+    await axios.post('http://localhost:4000/todos', newTodo)
   }
 
   const {
@@ -26,6 +34,10 @@ const App = () => {
   } = useQuery({
     queryKey: ['todos'],
     queryFn: fetchTodos
+  })
+
+  const { mutate } = useMutation({
+    mutationFn: addTodo
   })
 
   if (isPending) {
@@ -39,6 +51,19 @@ const App = () => {
   return (
     <div>
       <h3>TanStack Query</h3>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+
+          const newTodoObj = { title: todoItem, isDone: false }
+
+          // useMutation 로직 필요
+          mutate(newTodoObj)
+        }}
+      >
+        <input type="text" value={todoItem} onChange={(e) => setTodoItem(e.target.value)} />
+        <button>추가</button>
+      </form>
       <ul>
         {todos.map((todo) => {
           return (
