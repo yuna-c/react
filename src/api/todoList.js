92a12353fa20a5e14afd567eaed906f3172ae917
yuna <yuna744@gmail.com>
2024-09-08 00:52:12 +0900

    📝 add : queryKey 원복

--- src/api/todoList.js
+++ src/api/todoList.js
@@ -1,5 +1,11 @@
 import axios from 'axios'
 
+export const getTodos = async ({ signal }) => {
+  console.log('getTodos 호출')
+  const response = await axios.get('http://localhost:5000/todos', { signal })
+  return response.data
+}
+
 // queryFn 은 매개변수로 QueryFunctionContext 이란 객체 받음
 // export const getTodos = async ({ queryFnContext }) => {
 //   const { queryKey, pageParam, signal, meta } = queryFnContext
@@ -14,15 +20,15 @@ import axios from 'axios'
 // }
 
 // queryFn 은 매개변수로 QueryFunctionContext 이란 객체를 받지 않고, 개별 필드를 직접 받음
-export const getTodos = async ({ queryKey, signal }) => {
-  // queryKey: 배열 형태의 쿼리키
-  // signal: AbortSignal 을 의미 (네트워크 요청을 중간에 중단시킬 수 있는 장치)
-  console.log('getTodos 호출')
+// export const getTodos = async ({ queryKey, signal }) => {
+//   // queryKey: 배열 형태의 쿼리키
+//   // signal: AbortSignal 을 의미 (네트워크 요청을 중간에 중단시킬 수 있는 장치)
+//   console.log('getTodos 호출')
 
-  const response = await axios.get('http://localhost:5000/todos', { signal })
+//   const response = await axios.get('http://localhost:5000/todos', { signal })
 
-  return response.data
-}
+//   return response.data
+// }
 
 export const getTodo = async (id) => {
   console.log('getTodo 호출!')
