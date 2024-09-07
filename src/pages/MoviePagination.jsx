f3a697da1e8d5ba3f98c78559cb1412df520260d
yuna <yuna744@gmail.com>
2024-09-08 00:58:44 +0900

    📝 add : keepPreviousData vs Skeleton UI

--- src/pages/MoviePagination.jsx
+++ src/pages/MoviePagination.jsx
@@ -1,38 +1,39 @@
-import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
-import { fetchMovieData } from "../api/movie";
-import Pagination from "../components/Pagination";
+import { useQuery } from '@tanstack/react-query'
+import { useState } from 'react'
+import { fetchMovieData } from '../api/movie'
+import Pagination from '../components/Pagination'
 
 export default function MoviePagination() {
   // TMDB 에서 영화정보 가져와서 페이지네이션 적용하기
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(1)
   const { data: movies, isLoading } = useQuery({
-    queryKey: ["movies", page], // initial queryKey:["movie", 1]
+    queryKey: ['movies', page], // initial queryKey:["movie", 1]
     queryFn: fetchMovieData,
     select: ({ total_pages, results }) => ({
       total_pages,
-      results,
+      results
     }),
-    keepPreviousData: true,
-  });
-  console.log("movies:", movies);
+    // 원래 가지고 있던 데이터를 유지하다가 가지고오면 바꾸어서 깜빡임 없애줌
+    keepPreviousData: true
+  })
+  console.log('movies:', movies)
 
   const onClickPage = (selected) => {
     // 같은 페이지를 그대로 클릭시 함수종료
-    if (page === selected) return;
-    if (typeof selected === "number") {
-      setPage(selected);
-      return;
+    if (page === selected) return
+    if (typeof selected === 'number') {
+      setPage(selected)
+      return
     }
-    if (selected === "prev" && page > 1) {
-      setPage((prev) => prev - 1);
-      return;
+    if (selected === 'prev' && page > 1) {
+      setPage((prev) => prev - 1)
+      return
     }
-    if (selected === "next" && page < movies.total_pages) {
-      setPage((prev) => prev + 1);
-      return;
+    if (selected === 'next' && page < movies.total_pages) {
+      setPage((prev) => prev + 1)
+      return
     }
-  };
+  }
 
   return (
     <div>
@@ -47,11 +48,7 @@ export default function MoviePagination() {
         </ul>
       )}
 
-      <Pagination
-        currentPage={page}
-        totalPages={movies?.total_pages ?? 1}
-        onClick={onClickPage}
-      />
+      <Pagination currentPage={page} totalPages={movies?.total_pages ?? 1} onClick={onClickPage} />
     </div>
-  );
+  )
 }
