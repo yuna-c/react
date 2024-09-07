b82e207398e188391853ddb89c4997dbafc1f063
yuna <yuna744@gmail.com>
2024-09-08 01:23:23 +0900

    📝 add : 추가 세팅

---
+++ public/npm.js
@@ -0,0 +1,6 @@
+// yarn add json-server
+// yarn json-server db.json --port 4000
+// yarn add axios
+// yarn add @tanstack/react-query @tanstack/react-query-devtools
+// yarn add react-router-dom
+// yarn add react-intersection-observer
